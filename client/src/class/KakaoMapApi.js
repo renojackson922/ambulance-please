a30ee96d21c2d2fac07aa8d07bd1b695fc3129e6
Margarette <renojackson922@gmail.com>
2021-02-22 18:02:57 +0900

    Daily Commit

---
+++ client/src/class/KakaoMapApi.js
@@ -0,0 +1,68 @@
+/* global kakao */  // kakao 가 먼지 몰라도 그냥 뿌림
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import styled from "styled-components";
+
+
+
+// class KakaoMapApi extends Component {
+//   componentDidMount() {
+//     const script = document.createElement("script");
+//     script.async = true;
+//     script.src =
+//       "https://dapi.kakao.com/v2/maps/sdk.js?appkey=622a26bbe65339602e2bea28321bb212&autoload=false";
+//     document.head.appendChild(script);
+
+//     script.onload = () => {
+//       kakao.maps.load(() => {
+//         let container = document.getElementById("Mymap");
+//         let options = {
+//           center: new kakao.maps.LatLng(37.506502, 127.053617),
+//           level: 7
+//         };
+
+//         const map = new window.kakao.maps.Map(container, options);
+     
+//       });
+//     };
+//   }
+
+//   render() {
+//     return <KakaoMapApis id="Mymap"></KakaoMapApis>;
+//   }
+// }
+
+// const KakaoMapApis = styled.div`
+//   width: 100%;
+//   height: 100%;
+// `;
+
+class KakaoMapApi extends Component {
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src =
+      "https://dapi.kakao.com/v2/maps/sdk.js?appkey=622a26bbe65339602e2bea28321bb212&autoload=false";
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      kakao.maps.load(() => {
+        let container = document.getElementById("myMap");
+        let options = {
+          center: new kakao.maps.LatLng(37.506502, 127.053617),
+          level: 7
+        };
+
+        const map = new window.kakao.maps.Map(container, options);
+     
+      });
+    };
+  }
+
+   render(){
+    return <div id="myMap" style={{ width:'700px', height:'700px' }}></div>
+  }
+}
+
+
+export default KakaoMapApi;
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import Clock from './class/clock';
 import './index.css';
 import App from './App';
+import KakaoMapApi from './class/KakaoMapApi';
 import reportWebVitals from './reportWebVitals';
 
+// ReactDOM.render(
+//   // <React.StrictMode>
+//   //   <App />
+//   // </React.StrictMode>
+//   element,
+//   document.getElementById('root')
+// );
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+    render() {
+      return (
+        <div>
+          <h1>Hello, world!</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
+
+  function App2(){ 
+    return(
+      <div>
+        <Clock inteval="1000"/>
+        <Clock inteval="2000"/>
+        <Clock inteval="3000"/>
+      </div>
+    );
+  }
+
+  function Mp3Test(){
+    return(
+      <div>
+        <audio controls="controls" controlsList="nodownload">
+          <source src="https://docs.google.com/uc?export=download&id=1vunlaJfCuNHd34DduUTLU3s0wAu4PObG"/>
+        </audio>
+      </div>
+    )
+  }
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <span>응급</span>
+    <KakaoMapApi/>
+  </div>,
   document.getElementById('root')
 );
+
+  // <Mp3Test/>,
+  // <KakaoMapApi/>,
+  // <App2 />,
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
